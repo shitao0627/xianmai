7c79aee60753dff57462a7d27192cf4119e7addd
shitao0627 <50075261+shitao0627@users.noreply.github.com>
2021-12-31 10:16:54 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 公用样式 */
+	@import url("/static/iconfont/iconfont.css");
+	page{
+		background-color: #F9F9F9;
+		font-size:62.5%;
+	}
+	
+</style>
